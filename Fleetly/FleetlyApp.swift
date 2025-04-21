c69f55793bdc26f76b0aaa2b110edcd268961199
Himavarshith Reddy <himavarshithreddy@gmail.com>
2025-04-21 11:34:31 +0530

    Initial Commit

---
+++ Fleetly/FleetlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FleetlyApp.swift
+//  Fleetly
+//
+//  Created by admin68 on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FleetlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
