fba2da3211060fcdee796ff3c88c4549d37b2fb0
rhmt80 <113451102+rhmt80@users.noreply.github.com>
2025-05-02 14:00:42 +0530

    tickets view

--- Fleetly/Driver/DriverHomePage.swift
+++ Fleetly/Driver/DriverHomePage.swift
@@ -15,6 +15,10 @@ struct MainView: View {
                 .tabItem {
                     Label("Schedule", systemImage: "calendar")
                 }
+            TicketsView()
+                .tabItem {
+                    Label("Tickets", systemImage: "ticket.fill")
+                }
         }
         .toolbar {
             ToolbarItem(placement: .navigationBarTrailing) {
